62b450237f2af2c4cba4335aa57e7a0d66d9c8f9
arifrhm <arif.rahman.rhm@gmail.com>
2022-07-28 15:59:43 +0700

    Feat : Double Parsing for span

--- scrap_css_selector.py
+++ scrap_css_selector.py
@@ -13,8 +13,15 @@ webpage = requests.get(URL, headers=HEADERS)
 soup = BeautifulSoup(webpage.content, 'html.parser')
 
 # Find all by selector
-els = soup.select('#jobList > div.sx2jih0.z0qC4_0 > div:nth-child(2) > div')
+els = soup.select('#jobList > div.sx2jih0.z0qC4_0 > div:nth-child(2) > div ')
 
+el_list= str(els[0])#.text.split("/")
 
-for el in els:
-    print(el)
+soup2 = BeautifulSoup(el_list,'html.parser')
+elements = soup2.find_all("span")
+
+list_baru = []
+
+for element in elements:
+    print(element.text)
+    list_baru.append(element.text)
